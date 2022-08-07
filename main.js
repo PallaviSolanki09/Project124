0a7756aeff4a3eb49f0eff92f7ce8b4cd6b94c97
PallaviSolanki09 <89520116+PallaviSolanki09@users.noreply.github.com>
2022-08-07 19:25:59 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+function setup(){
+    video=createCapture(VIDEO);
+    video.size(400,400);
+    video.position(10,50);
+
+    canvas = createCanvas(800,400);
+    canvas.position(430,130);
+
+    poseNet = ml5.poseNet(video,modalloaded);
+    poseNet.on('pose',gotposes);
+}
+
+function draw(){
+    background("#98b2e6");
+}
+
+function modalloaded(){
+    console.log("PoseNet Is Initialized And Loaded");
+}
+
+function gotposes(results,error){
+    if(error){
+        console.error(error);
+    }
+    if(results.length > 0){
+        console.log(results);
+    }
+}
